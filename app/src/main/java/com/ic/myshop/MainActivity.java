af2937048f2aa653004d55a6b351e628d1f52784
AnhNH <anhnh11032001@gmail.com>
2023-10-08 01:43:48 +0700

    Add icon app + icon launcher + event first-time to app

--- app/src/main/java/com/ic/myshop/MainActivity.java
+++ app/src/main/java/com/ic/myshop/MainActivity.java
@@ -2,13 +2,53 @@ package com.ic.myshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String MAP = "map";
+    private final static String IS_FIRST_TIME = "isFirstTime";
+
+    private Button start_btn;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        Intent intent = new Intent(this, MainActivity2.class);
+        if (isFirstTime()) {
+            start_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(intent);
+                    finish();
+                }
+            });
+            updateFirstTime();
+        } else {
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void init() {
+        start_btn = findViewById(R.id.start_btn);
+        sharedPreferences = getSharedPreferences(MAP, MODE_PRIVATE);
+    }
+
+    private boolean isFirstTime() {
+        return sharedPreferences.getBoolean(IS_FIRST_TIME, true);
+    }
+
+    private void updateFirstTime() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(IS_FIRST_TIME, false);
+        editor.apply();
     }
 }

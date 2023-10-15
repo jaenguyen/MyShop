c40893795658e5676460c92e7ff32abe54f6e0cc
AnhNH <anhnh11032001@gmail.com>
2023-10-15 21:41:48 +0700

    Thêm giao diện đăng nhập, đăng ký, quên mật khẩu

---
+++ app/src/main/java/com/ic/myshop/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.ic.myshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // TODO: XỬ LÝ GIAO DIỆN
+    }
+}

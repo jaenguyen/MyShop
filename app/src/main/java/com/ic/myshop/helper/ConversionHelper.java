5a6511eb3a6d6863301221a584b377489408dd6f
AnhNH <anhnh11032001@gmail.com>
2023-10-29 23:19:53 +0700

    Giao diện sản phẩm

--- app/src/main/java/com/ic/myshop/helper/ConversionHelper.java
+++ app/src/main/java/com/ic/myshop/helper/ConversionHelper.java
@@ -1,5 +1,11 @@
 package com.ic.myshop.helper;
 
+import java.text.DecimalFormat;
+
 public class ConversionHelper {
 
+    public static String formatNumber(long number) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(number);
+    }
 }

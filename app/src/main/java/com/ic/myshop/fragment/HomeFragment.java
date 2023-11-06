e77b2bf2448154466b8ba615661b583ed88eb406
AnhNH <anhnh11032001@gmail.com>
2023-11-06 10:01:41 +0700

    giao diện home

--- app/src/main/java/com/ic/myshop/fragment/HomeFragment.java
+++ app/src/main/java/com/ic/myshop/fragment/HomeFragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,11 +20,21 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.ic.myshop.R;
 import com.ic.myshop.activity.CartActivity;
+import com.ic.myshop.adapter.SliderAdapter;
 import com.ic.myshop.constant.DatabaseConstant;
 import com.ic.myshop.model.Like;
+import com.ic.myshop.output.Slider;
+
+import me.relex.circleindicator.CircleIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private ViewPager viewPagerSlider;
+    private CircleIndicator circleIndicator;
+    private SliderAdapter sliderAdapter;
     private ImageView btnCart;
     private FirebaseFirestore db;
 
@@ -38,6 +49,11 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        viewPagerSlider = view.findViewById(R.id.view_pager_slider);
+        circleIndicator = view.findViewById(R.id.circle_indicator);
+        sliderAdapter = new SliderAdapter(getContext(), getSliderPhoto());
+        viewPagerSlider.setAdapter(sliderAdapter);
+        sliderAdapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
         btnCart = view.findViewById(R.id.btn_cart);
         db = FirebaseFirestore.getInstance();
         db.collection(DatabaseConstant.LIKES)
@@ -63,5 +79,13 @@ public class HomeFragment extends Fragment {
                 startActivity(intentCart);
             }
         });
+    }
+
+    private List<Slider> getSliderPhoto() {
+        List<Slider> sliders = new ArrayList<>();
+        sliders.add(new Slider(R.drawable.slider1));
+        sliders.add(new Slider(R.drawable.slider2));
+        sliders.add(new Slider(R.drawable.slider3));
+        return sliders;
     }
 }

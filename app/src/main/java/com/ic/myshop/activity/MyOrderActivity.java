248def9bc680f69ae9693c1d5405ab1757c540a6
AnhNH <anhnh11032001@gmail.com>
2023-11-07 19:20:25 +0700

    order + tablayout

---
+++ app/src/main/java/com/ic/myshop/activity/MyOrderActivity.java
@@ -0,0 +1,50 @@
+package com.ic.myshop.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+import com.ic.myshop.R;
+import com.ic.myshop.constant.Constant;
+import com.ic.myshop.fragment.ViewPagerOderAdapter;
+
+public class MyOrderActivity extends AppCompatActivity {
+
+    private TextView toolbarTitle;
+    private ImageButton btnBack;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_order);
+        init();
+
+        ViewPagerOderAdapter viewPagerOderAdapter = new ViewPagerOderAdapter(getSupportFragmentManager(),
+                FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerOderAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void init() {
+        toolbarTitle = findViewById(R.id.toolbar_title);
+        toolbarTitle.setText(Constant.MY_ORDER);
+        btnBack = findViewById(R.id.toolbar_back_button);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+    }
+}
--- app/src/main/java/com/ic/myshop/constant/Constant.java
+++ app/src/main/java/com/ic/myshop/constant/Constant.java
@@ -19,4 +19,5 @@ public class Constant {
     public static final String EDIT_ADDRESS = "Sửa địa chỉ";
     public static final String LIKE_PRODUCTS = "Sản phẩm yêu thích";
     public static final String LIST_PRODUCT = "Danh sách sản phẩm";
+    public static final String MY_ORDER = "Đơn hàng của tôi";
 }

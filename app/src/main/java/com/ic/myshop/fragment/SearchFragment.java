2e0d683776fbf1658a291d3c60912db0a6ea4f3a
AnhNH <anhnh11032001@gmail.com>
2023-11-10 18:38:48 +0700

    prototype search

--- app/src/main/java/com/ic/myshop/fragment/SearchFragment.java
+++ app/src/main/java/com/ic/myshop/fragment/SearchFragment.java
@@ -6,12 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,28 +21,25 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.ic.myshop.R;
 import com.ic.myshop.activity.CartActivity;
-import com.ic.myshop.adapter.ProductAdapter;
+import com.ic.myshop.adapter.SearchProductAdapter;
 import com.ic.myshop.constant.DatabaseConstant;
-import com.ic.myshop.constant.InputParam;
 import com.ic.myshop.db.DbFactory;
 import com.ic.myshop.model.Product;
 
 public class SearchFragment extends Fragment {
 
     private ImageView btnCart;
-    private ProgressBar progressBar;
     private RecyclerView rcvProduct;
-    private GridLayoutManager layoutManager;
-    private ProductAdapter productAdapter;
-    private long maxScore = Long.MAX_VALUE;
-    private boolean isScrolling = false;
+    protected LinearLayoutManager linearLayoutManager;
+    private SearchProductAdapter productAdapter;
+    private SearchView searchView;
     private FirebaseFirestore db;
     private static final DbFactory dbFactory = DbFactory.getInstance();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
         return view;
     }
 
@@ -50,13 +47,13 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        searchView = view.findViewById(R.id.search_view);
         btnCart = view.findViewById(R.id.btn_cart);
         db = FirebaseFirestore.getInstance();
-        progressBar = view.findViewById(R.id.progress_bar);
-        rcvProduct = view.findViewById(R.id.rcv_product);
-        layoutManager = new GridLayoutManager(getContext(), 2);
-        rcvProduct.setLayoutManager(layoutManager);
-        productAdapter = new ProductAdapter(getContext());
+        rcvProduct = view.findViewById(R.id.rcv_search);
+        linearLayoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        rcvProduct.setLayoutManager(linearLayoutManager);
+        productAdapter = new SearchProductAdapter(getContext());
         rcvProduct.setAdapter(productAdapter);
 
         Intent intentCart = new Intent(getContext(), CartActivity.class);
@@ -68,81 +65,55 @@ public class SearchFragment extends Fragment {
             }
         });
 
-        db.collection(DatabaseConstant.PRODUCTS)
-//                .orderBy(InputParam.CREATED_TIME, Query.Direction.DESCENDING)
-//                .startAt(Long.MAX_VALUE)
-//                .limit(4)
-                .whereNotEqualTo("parentId", dbFactory.getUserId())
-                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Product product = document.toObject(Product.class);
-                                productAdapter.addProduct(product);
-                            }
-                            if (!task.getResult().isEmpty()) {
-                                maxScore = task.getResult().getDocuments().
-                                        get(task.getResult().size() - 1).getLong(InputParam.CREATED_TIME);
-                            }
-                        } else {
-                        }
-                    }
-                });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (query.length() > 0) {
+                    db.collection(DatabaseConstant.PRODUCTS)
+                            .whereGreaterThanOrEqualTo("name", query)
+                            .whereLessThanOrEqualTo("name", query + "~")
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        productAdapter.clear();
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            Product product = document.toObject(Product.class);
+                                            productAdapter.addProduct(product);
+                                        }
+                                    } else {
+                                    }
+                                }
+                            });
+                }
+                return false;
+            }
 
-//        rcvProduct.addOnScrollListener(new RecyclerView.OnScrollListener() {
-//            @Override
-//            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-//                super.onScrollStateChanged(recyclerView, newState);
-//                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
-//                    isScrolling = true;
-//            }
-//
-//            @Override
-//            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-//                super.onScrolled(recyclerView, dx, dy);
-//
-//                int currentItem = layoutManager.getChildCount();
-//                int totalItemCount = layoutManager.getItemCount();
-//                int scrollOutItem = layoutManager.findFirstVisibleItemPosition();
-//
-//                if (isScrolling && currentItem + scrollOutItem == totalItemCount) {
-//                    isScrolling = false;
-//                    loadMoreProduct();
-//                }
-//            }
-//        });
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.length() > 0) {
+                    db.collection(DatabaseConstant.PRODUCTS)
+                            .whereGreaterThanOrEqualTo("name", newText)
+                            .whereLessThanOrEqualTo("name", newText + '\uf8ff')
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        productAdapter.clear();
+
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            Product product = document.toObject(Product.class);
+                                            productAdapter.addProduct(product);
+                                        }
+                                    } else {
+                                    }
+                                }
+                            });
+                }
+                return false;
+            }
+        });
     }
-
-//    private void loadMoreProduct() {
-//        progressBar.setVisibility(View.VISIBLE);
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                db.collection(DatabaseConstant.PRODUCTS)
-//                        .orderBy(InputParam.CREATED_TIME, Query.Direction.DESCENDING)
-//                        .startAfter(maxScore)
-//                        .limit(4)
-//                        .get()
-//                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                            @Override
-//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                                if (task.isSuccessful()) {
-//                                    for (QueryDocumentSnapshot document : task.getResult()) {
-//                                        Product product = document.toObject(Product.class);
-//                                        productAdapter.addProduct(product);
-//                                    }
-//                                    progressBar.setVisibility(View.GONE);
-//                                    if (!task.getResult().isEmpty()) {
-//                                        maxScore = task.getResult().getDocuments().
-//                                                get(task.getResult().size() - 1).getLong(InputParam.CREATED_TIME);
-//                                    }
-//                                } else {
-//                                    isScrolling = false;
-//                                }
-//                            }
-//                        });
-//            }
-//        }, 1000);
-//    }
 }
